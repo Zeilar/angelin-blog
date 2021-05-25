114f4c3c904e1335924c5195d1569121995ea8b4
Philip <philip_angelin@hotmail.com>
2021-05-25 07:53:58 +0200

    Postscontroller

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -1,5 +1,19 @@
+const { Post } = require("../../db/models/Post");
+
 async function create(req, res) {}
+
+async function getPostsFromAuthorId(req, res) {
+	const { user_id } = req.body;
+	try {
+		const posts = await Post.query().where("user_id", user_id);
+		res.status(200).json(posts);
+	} catch (e) {
+		console.log(e);
+		res.status(500).end();
+	}
+}
 
 module.exports = {
 	create,
+	getPostsFromAuthorId,
 };
