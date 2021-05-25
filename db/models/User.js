a7fdb2b2242f1cfba56b30c86be5a846954effd3
Philip <philip_angelin@hotmail.com>
2021-05-25 07:53:12 +0200

    Fixed circular dependency

--- db/models/User.js
+++ db/models/User.js
@@ -1,21 +1,23 @@
 const { Model } = require("objection");
-const { Post } = require("./Post");
 
 class User extends Model {
 	static get tableName() {
 		return "users";
 	}
 
-	static relationMappings = {
-		posts: {
-			relation: Model.HasManyRelation,
-			modelClass: Post,
-			join: {
-				from: "users.id",
-				to: "posts.user_id",
+	static get relationMappings() {
+		const Post = require("./Post");
+		return {
+			posts: {
+				relation: Model.HasManyRelation,
+				modelClass: Post,
+				join: {
+					from: "users.id",
+					to: "posts.user_id",
+				},
 			},
-		},
-	};
+		};
+	}
 }
 
 module.exports = {
