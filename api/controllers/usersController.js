66923cd8558827aa1d435e348ae04461e69d29d5
Philip <philip_angelin@hotmail.com>
2021-05-26 18:51:43 +0200

    Refactoring, logging in user when registering

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -3,10 +3,21 @@ const { User } = require("../../db/models/User");
 const errorlog = require("../utils/errorlog");
 const { userCount } = require("../utils/user");
 
-function authenticate(req, res) {
-	const { user } = req.session;
-	if (user) return res.status(200).json({ id: user });
-	res.status(401).end();
+async function authenticate(req, res) {
+	const { user: id } = req.session;
+
+	if (!id) {
+		return res.status(401).end();
+	}
+
+	try {
+		const user = await User.query().findById(id);
+		user.password = undefined;
+		return res.status(200).json(user);
+	} catch (e) {
+		errorlog(e);
+		return res.status(500).end();
+	}
 }
 
 async function register(req, res) {
@@ -22,10 +33,12 @@ async function register(req, res) {
 		}
 
 		// TODO: Validation
-		await User.query().insert({
+		const { id } = await User.query().insert({
 			email,
 			password: await bcrypt.hash(password, 10),
 		});
+
+		req.session.user = id;
 
 		res.status(200).end();
 	} catch (e) {
@@ -38,17 +51,21 @@ async function login(req, res) {
 	const { email, password } = req.body;
 
 	if (!email || !password) {
-		return res.status(400).json({ error: "Missing email or password" });
+		return res.status(400).json({ error: "Missing email or password." });
+	}
+
+	if (req.session.user) {
+		return res.status(405).json({ error: "You are already logged in." });
 	}
 
 	try {
 		const user = await User.query().findOne("email", email);
 		if (!user) {
-			return res.status(404).json({ error: "User does not exist" });
+			return res.status(404).json({ error: "User does not exist." });
 		}
 
 		if (!(await bcrypt.compare(password, user.password))) {
-			return res.status(422).json({ error: "Incorrect password" });
+			return res.status(422).json({ error: "Incorrect password." });
 		}
 
 		user.password = undefined;
