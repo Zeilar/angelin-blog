112838c96e7fdb0228b17407f871fb7d45fbd8c5
Philip <philip_angelin@hotmail.com>
2021-05-26 18:41:56 +0200

    Refactored userCount

--- api/utils/user.js
+++ api/utils/user.js
@@ -1,12 +1,18 @@
 const { User } = require("../../db/models/User");
+const errorlog = require("./errorlog");
 
 async function userCount(options) {
-	let query = User.query();
-	if (options.where) {
-		query = query.where({ ...options.where });
+	try {
+		let query = User.query();
+		if (options.where) {
+			query = query.where({ ...options.where });
+		}
+		const { count } = await query.count(["id", { as: "count" }]).first();
+		return count;
+	} catch (e) {
+		errorlog(e);
+		return 0;
 	}
-	const result = await query.count(["id", { as: "count" }]);
-	return result[0].count;
 }
 
 module.exports = {
