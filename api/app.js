9007c1d12e1121fc736a529e9717957bdb3d1c83
Philip <philip_angelin@hotmail.com>
2021-05-28 16:58:35 +0200

    Removed dummy function

--- api/app.js
+++ api/app.js
@@ -8,9 +8,6 @@ const { PORT, SESSION_SECRET } = process.env;
 const usersRoutes = require("./routes/usersRoutes");
 const postsRoutes = require("./routes/postsRoutes");
 const tagsRoutes = require("./routes/tagsRoutes");
-const { Post } = require("../db/models/Post");
-const { Comment } = require("../db/models/Comment");
-const { User } = require("../db/models/User");
 
 // Middlewares
 app.use(express.json());
@@ -26,14 +23,5 @@ app.use(
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/tags", tagsRoutes);
-
-app.get("/test", async (req, res) => {
-	const s = await User.query().withGraphFetched({
-		posts: {
-			comments: true,
-		},
-	});
-	res.json(s);
-});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
