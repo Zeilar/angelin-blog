dc446091fd2babb55e90da8e462479eb20f5200b
Philip <philip_angelin@hotmail.com>
2021-05-31 16:20:27 +0200

    Reverted logic on getCommentOrFail

--- server/api/middlewares/comment/request.ts
+++ server/api/middlewares/comment/request.ts
@@ -8,11 +8,22 @@ export async function getCommentOrFail(
 	res: Response,
 	next: NextFunction
 ): Promise<void> {
+	const { id } = req.params;
+
 	try {
-		const comments: Comment[] | [] = await Comment.query().withGraphFetched(
-			Comment.relationships
-		);
-		res.comments = comments.map((comment: Comment) => sanitizeComment(comment));
+		if (id) {
+			const comment: Comment | null = await Comment.query()
+				.findById(id)
+				.withGraphFetched(Comment.relationships);
+			if (!comment) return res.status(404).end();
+
+			res.comment = sanitizeComment(comment);
+		} else {
+			const comments: Comment[] | [] = await Comment.query().withGraphFetched(
+				Comment.relationships
+			);
+			res.comments = comments.map((comment: Comment) => sanitizeComment(comment));
+		}
 
 		next();
 	} catch (error) {
