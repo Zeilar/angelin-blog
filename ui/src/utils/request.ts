a98c611cc783ecf9e0ea75989fc860eb1fd4343b
Philip <philip_angelin@hotmail.com>
2021-06-07 09:00:31 +0200

    Added parseQueryParams to Request

--- ui/src/utils/request.ts
+++ ui/src/utils/request.ts
@@ -1,15 +1,30 @@
-import { Args } from "../types/request";
+import { Args, QueryParams } from "../types/request";
 import { SERVER_URL } from "./constants";
 
 export default class Request {
+	private static parseQueryParams(params?: QueryParams): string {
+		if (!params || !Object.keys(params).length) {
+			return "";
+		}
+
+		// Parse { key: value } to `key=value` strings
+		const queries = Object.entries(params)
+			.map(([key, val]) => `${key}=${val}`)
+			.join("&");
+
+		return `?${queries}`;
+	}
+
 	private static async request<T>(args: Args) {
+		const queries = Request.parseQueryParams(args.params);
+
 		let data: T | null = null;
 		let code: number = 200;
 		try {
-			const response = await fetch(args.url, {
+			const response = await fetch(`${args.url}${queries}`, {
 				method: args.method,
 				body: JSON.stringify(args.body),
-				headers: { "Content-Type": "application/json" },
+				headers: { "Content-Type": "application/json", ...args.headers },
 			});
 
 			code = response.status;
