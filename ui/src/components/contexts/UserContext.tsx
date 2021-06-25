7af014b45d5c4835a68d00c94859ccb783d2147a
Philip <philip_angelin@hotmail.com>
2021-06-25 21:59:05 +0200

    No reason not to update user state instantly

--- ui/src/components/contexts/UserContext.tsx
+++ ui/src/components/contexts/UserContext.tsx
@@ -49,8 +49,8 @@ export function UserContextProvider({ children }: Props) {
 	}
 
 	async function logout() {
-		const { code, error } = await User.logout();
 		setUser(null);
+		const { code, error } = await User.logout();
 		return { code, error };
 	}
 
