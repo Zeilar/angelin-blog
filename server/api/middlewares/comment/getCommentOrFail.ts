b05f6b95e4bdf9232f94b9f55b0fa1dc8bbb9571
Philip <philip_angelin@hotmail.com>
2021-06-30 14:04:34 +0200

    Paginating comments

--- server/api/middlewares/comment/getCommentOrFail.ts
+++ server/api/middlewares/comment/getCommentOrFail.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Comment } from "../../../db/models";
 import errorlog from "../../../utils/errorlog";
+import { NumberHelpers, PAGE_SIZE } from "../../utils";
 
 export async function getCommentOrFail(req: Request, res: Response, next: NextFunction) {
 	const { id } = req.params;
@@ -12,7 +13,18 @@ export async function getCommentOrFail(req: Request, res: Response, next: NextFu
 			if (!comment) return res.status(404).end();
 			res.comment = comment.sanitize();
 		} else {
-			res.comments = await Comment.query().withGraphFetched(Comment.relationships);
+			const { page, perPage } = req.query;
+
+			const pagination = NumberHelpers.paginate(
+				page as string,
+				(perPage as string) ?? PAGE_SIZE
+			);
+
+			res.comments = (
+				await Comment.query()
+					.withGraphFetched(Comment.relationships)
+					.page(pagination.page, pagination.perPage)
+			).results;
 		}
 		next();
 	} catch (error) {
