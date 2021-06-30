a93b3be94c35b1ef251e21b3c2acbdf3374b5600
Philip <philip_angelin@hotmail.com>
2021-06-30 14:03:19 +0200

    Paginating users

--- server/api/middlewares/post/getPostOrFail.ts
+++ server/api/middlewares/post/getPostOrFail.ts
@@ -5,18 +5,19 @@ import { PAGE_SIZE, NumberHelpers } from "../../utils";
 
 export async function getPostOrFail(req: Request, res: Response, next: NextFunction) {
 	const { id } = req.params;
-	const { page, perPage } = req.query;
 	try {
 		if (id) {
 			const post = await Post.query().findById(id).withGraphFetched(Post.relationships);
 			if (!post) return res.status(404).end();
 			res.post = post.sanitize();
 		} else {
+			const { page, perPage } = req.query;
+
 			const pagination = NumberHelpers.paginate(
 				page as string,
 				(perPage as string) ?? PAGE_SIZE
 			);
-			console.log(pagination);
+
 			res.posts = (
 				await Post.query()
 					.withGraphFetched(Post.relationships)
