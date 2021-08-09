dbda9eac0cb826802f2e10b797d023c5ddc52af7
Philip <philip_angelin@hotmail.com>
2021-08-09 21:08:39 +0200

    Middleware container refactoring

--- server/api/middlewares/Middleware.ts
+++
@@ -1,9 +0,0 @@
-import { UserRepository } from "../../repositories";
-import errorlog from "../../utils/errorlog";
-import { ErrorMessages } from "../utils";
-
-export class Middleware {
-	public readonly userRepository = new UserRepository();
-	public readonly errorlog = errorlog;
-	public readonly ErrorMessages = ErrorMessages;
-}
--- server/api/middlewares/user/getUserOrFail.ts
+++ server/api/middlewares/user/getUserOrFail.ts
@@ -1,17 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { container } from "../../../bootstrap";
 import { UserRepository } from "../../../repositories";
 import errorlog from "../../../utils/errorlog";
 import { ErrorMessages } from "../../utils";
 
 export async function getUserOrFail(req: Request, res: Response, next: NextFunction) {
+	const userRepository = container.get(UserRepository);
 	const { id } = req.params;
+
 	try {
 		if (!id) {
 			res.status(400).json({ error: "Expected id query parameter." });
 			return;
 		}
 
-		const user = await new UserRepository().findById(id);
+		const user = await userRepository.findById(id);
 
 		if (!user) {
 			res.status(404).json({ error: ErrorMessages.NOT_FOUND });
